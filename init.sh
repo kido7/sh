a5f0759452a2177d30bc1f233e3702f5ed30a413
kido7 <kido@izdatelstvo-sk.kz>
2018-12-08 00:47:02 +0600

    gunicorn

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/hello.py
+sudo /etc/init.d/nginx stop
+sudo /etc/init.d/mysql stop
+sudo /etc/init.d/gunicorn stop
+
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
+sudo /etc/init.d/nginx start
+
